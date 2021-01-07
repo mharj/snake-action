a74871e33f0e1756e18196a943db4fea4d642385
Marko Harjula <marko.harjula@ajrsolutions.fi>
2021-01-07 13:59:27 +0000

    base testing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,39 @@
-export abstract class SnakeAction {
+export type SnakeConstructor<P = {}, S = {}> = new (...args: P[]) => SnakeAction<P, S>;
+type extractGeneric<Type> = Type extends SnakeAction<infer X> ? X : never;
+
+export abstract class SnakeAction<P = {}, S = {}> {
+	private runned = false;
+	private reverted = false;
+	protected state: S;
+	protected props: P;
+	constructor(props: P) {
+		this.props = props;
+	}
 	public abstract onAction(): Promise<void>;
 	public abstract onException(): Promise<void>;
+	public getState(): Readonly<S> {
+		return this.state;
+	}
+	public getProps(): Readonly<P> {
+		return this.props;
+	}
+	protected setState<K extends keyof S>(newState: Pick<S, K> | S | null) {
+		if (newState) {
+			this.state = {...this.state, ...newState};
+		}
+	}
+	public setRunned(value: boolean) {
+		this.runned = value;
+	}
+	public didRun() {
+		return this.runned;
+	}
+	public setReverted(value: boolean) {
+		this.reverted = value;
+	}
+	public didRevert() {
+		return this.reverted;
+	}
 }
 
 interface ICommitOptions {
@@ -8,12 +41,14 @@ interface ICommitOptions {
 }
 
 export class SnakeBuilder {
-	protected instances: SnakeAction[] = [];
+	private instances: SnakeAction[] = [];
 	protected didRun: SnakeAction[] = [];
+
 	public async commit(options?: ICommitOptions) {
 		try {
 			for (const instance of this.instances) {
 				await instance.onAction();
+				instance.setRunned(true);
 				this.didRun.unshift(instance);
 			}
 		} catch (err) {
@@ -24,10 +59,24 @@ export class SnakeBuilder {
 		if (options && options.rollBack) {
 			await this.rollBack();
 		}
+		return this.instances;
+	}
+
+	public getInstances(): SnakeAction[] {
+		return this.instances;
+	}
+
+	public add<C extends SnakeConstructor, P = extractGeneric<C>>(ctor: C, args: P) {
+		this.addInstance(new ctor(args));
+		return this;
+	}
+	protected addInstance(action: SnakeAction) {
+		this.instances.push(action);
 	}
 	private async rollBack() {
 		for (const instance of this.didRun) {
 			await instance.onException();
+			instance.setReverted(true);
 		}
 		this.didRun = []; // clear rollBack instances
 	}
--- test/testSnake.ts
+++ test/testSnake.ts
@@ -1,32 +1,169 @@
+import {expect} from 'chai';
 import {SnakeAction, SnakeBuilder} from '../src';
 
-class TestAction implements SnakeAction {
-	private didRun = false;
-	private state = false;
-	constructor(demo: string) {
-		//
+class TestAction extends SnakeAction<string, {value: boolean}> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			value: false,
+		};
 	}
 	public onAction(): Promise<void> {
-		this.didRun = true;
-		this.state = true;
+		this.setState({
+			value: true,
+		});
 		return Promise.resolve();
 	}
 	public onException(): Promise<void> {
-		this.state = false;
+		this.setState({
+			value: false,
+		});
 		return Promise.resolve();
 	}
 }
-type TestActionProps = ConstructorParameters<typeof TestAction>;
 
-class DemoSnake extends SnakeBuilder {
-	public test(...args: TestActionProps): DemoSnake {
-		this.instances.push(new TestAction(...args));
-		return this;
+const isTestAction = (action: SnakeAction): action is TestAction => {
+	return action instanceof TestAction;
+};
+
+class Test2Action extends SnakeAction<number, {value: boolean}> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			value: false,
+		};
+	}
+	public onAction(): Promise<void> {
+		this.setState({
+			value: true,
+		});
+		return Promise.resolve();
+	}
+	public onException(): Promise<void> {
+		this.setState({
+			value: false,
+		});
+		return Promise.resolve();
 	}
 }
-async function Test() {
-	const snake = new DemoSnake();
-	await snake.test('asd').test('qwe').commit();
+
+const isTest2Action = (action: SnakeAction): action is TestAction => {
+	return action instanceof TestAction;
+};
+
+class TestExceptionAction extends SnakeAction<number, {value: boolean}> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			value: false,
+		};
+	}
+	public onAction(): Promise<void> {
+		throw new Error('demo');
+	}
+	public onException(): Promise<void> {
+		return Promise.resolve();
+	}
 }
 
-Test();
+const isTestExceptionAction = (action: SnakeAction): action is TestExceptionAction => {
+	return action instanceof TestExceptionAction;
+};
+
+describe('snake', () => {
+	it('should run 3 actions', async () => {
+		const snake = new SnakeBuilder();
+		await snake.add(TestAction, 'asd').add(TestAction, 'qwe').add(Test2Action, 1).commit();
+		const data = snake.getInstances();
+		expect(data.length).to.be.eq(3);
+		const [test1, test2, test3] = data;
+		expect(test1 instanceof TestAction).to.be.eq(true);
+		if (isTestAction(test1)) {
+			expect(test1.getState()).to.be.eql({value: true});
+			expect(test1.getProps()).to.be.eq('asd');
+			expect(test1.didRun()).to.be.eq(true);
+			expect(test1.didRevert()).to.be.eq(false);
+		}
+		expect(test2 instanceof TestAction).to.be.eq(true);
+		if (isTestAction(test2)) {
+			expect(test2.getState()).to.be.eql({value: true});
+			expect(test2.getProps()).to.be.eq('qwe');
+			expect(test2.didRun()).to.be.eq(true);
+			expect(test2.didRevert()).to.be.eq(false);
+		}
+		expect(test3 instanceof Test2Action).to.be.eq(true);
+		if (isTest2Action(test3)) {
+			expect(test3.getState()).to.be.eql({value: true});
+			expect(test3.getProps()).to.be.eq(1);
+			expect(test3.didRun()).to.be.eq(true);
+			expect(test3.didRevert()).to.be.eq(false);
+		}
+	});
+	it('should run 3 actions and revert', async () => {
+		const snake = new SnakeBuilder();
+		await snake.add(TestAction, 'asd').add(TestAction, 'qwe').add(Test2Action, 1).commit({rollBack: true});
+		const data = snake.getInstances();
+		expect(data.length).to.be.eq(3);
+		const [test1, test2, test3] = data;
+		expect(test1 instanceof TestAction).to.be.eq(true);
+		if (isTestAction(test1)) {
+			expect(test1.getState()).to.be.eql({value: false});
+			expect(test1.getProps()).to.be.eq('asd');
+			expect(test1.didRun()).to.be.eq(true);
+			expect(test1.didRevert()).to.be.eq(true);
+		}
+		expect(test2 instanceof TestAction).to.be.eq(true);
+		if (isTestAction(test2)) {
+			expect(test2.getState()).to.be.eql({value: false});
+			expect(test2.getProps()).to.be.eq('qwe');
+			expect(test2.didRun()).to.be.eq(true);
+			expect(test2.didRevert()).to.be.eq(true);
+		}
+		expect(test3 instanceof Test2Action).to.be.eq(true);
+		if (isTest2Action(test3)) {
+			expect(test3.getState()).to.be.eql({value: false});
+			expect(test3.getProps()).to.be.eq(1);
+			expect(test3.didRun()).to.be.eq(true);
+			expect(test3.didRevert()).to.be.eq(true);
+		}
+	});
+	it('should throw exception on last phase', async () => {
+		const snake = new SnakeBuilder();
+		try {
+			await snake.add(TestAction, 'asd').add(TestAction, 'qwe').add(Test2Action, 1).add(TestExceptionAction, 2).commit();
+			throw new Error('should not happen');
+		} catch (ex) {
+			const data = snake.getInstances();
+			expect(data.length).to.be.eq(4);
+			const [test1, test2, test3, test4] = data;
+			expect(test1 instanceof TestAction).to.be.eq(true);
+			if (isTestAction(test1)) {
+				expect(test1.getState()).to.be.eql({value: false});
+				expect(test1.getProps()).to.be.eq('asd');
+				expect(test1.didRun()).to.be.eq(true);
+				expect(test1.didRevert()).to.be.eq(true);
+			}
+			expect(test2 instanceof TestAction).to.be.eq(true);
+			if (isTestAction(test2)) {
+				expect(test2.getState()).to.be.eql({value: false});
+				expect(test2.getProps()).to.be.eq('qwe');
+				expect(test2.didRun()).to.be.eq(true);
+				expect(test2.didRevert()).to.be.eq(true);
+			}
+			expect(test3 instanceof Test2Action).to.be.eq(true);
+			if (isTest2Action(test3)) {
+				expect(test3.getState()).to.be.eql({value: false});
+				expect(test3.getProps()).to.be.eq(1);
+				expect(test3.didRun()).to.be.eq(true);
+				expect(test3.didRevert()).to.be.eq(true);
+			}
+			expect(test4 instanceof TestExceptionAction).to.be.eq(true);
+			if (isTestExceptionAction(test4)) {
+				expect(test4.getState()).to.be.eql({value: false});
+				expect(test4.getProps()).to.be.eq(2);
+				expect(test4.didRun()).to.be.eq(false);
+				expect(test4.didRevert()).to.be.eq(false);
+			}
+		}
+	});
+});
